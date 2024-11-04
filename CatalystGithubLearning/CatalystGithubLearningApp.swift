8a108b25f442fe5b8008cd9e54cef4e036754a8e
Jan Armbrust <jan.armbrust@googlemail.com>
2024-11-04 12:06:31 +0100

    Initial Commit

---
+++ CatalystGithubLearning/CatalystGithubLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatalystGithubLearningApp.swift
+//  CatalystGithubLearning
+//
+//  Created by Jan Armbrust on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CatalystGithubLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatalystGithubLearning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CatalystGithubLearning
+//
+//  Created by Jan Armbrust on 04/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
